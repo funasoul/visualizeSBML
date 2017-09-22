a4dc290c8b5356bd5cc0fd5841288d47bbd92e49
Akira Funahashi <funa@bio.keio.ac.jp>
2017-09-22 19:04:55 +0900

    Changed layout algorithm. Fixed indent

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-fetch('data.json', {mode: 'no-cors'})
+fetch('model.json', {mode: 'no-cors'})
   .then(function(res) {
     return res.json()
   })
@@ -10,74 +10,74 @@ fetch('data.json', {mode: 'no-cors'})
       autounselectify: true,
 
       layout: {
-        name: 'breadthfirst',
-		directed: true,
-		padding: 10,
-        animate: false
+        name: 'cose',
+        directed: true,
+        padding: 10,
+        animate: true
       },
 
       style: [
         {
           selector: 'node',
           style: {
-			'shape': 'roundrectangle',
+            'shape': 'roundrectangle',
             'background-color': '#d5fcd7',
             'border-color': 'black',
             'border-width': 1,
-			'width': 80,
-			'height': 40,
-			'text-valign': 'center',
-			'font-size': 12,
-			'label': 'data(name)'
+            'width': 80,
+            'height': 40,
+            'text-valign': 'center',
+            'font-size': 12,
+            'label': 'data(name)'
           }
         },
 
         {
           selector: 'node.rxn',
           style: {
-			'shape': 'rectangle',
+            'shape': 'rectangle',
             'background-color': 'white',
-			'width': 10,
-			'height': 10,
-			'label': ''
+            'width': 10,
+            'height': 10,
+            'label': ''
           }
         },
 
         {
           selector: 'edge',
-		  style: {
-			'curve-style': 'bezier',
+          style: {
+            'curve-style': 'bezier',
             'width': 1,
-			'target-arrow-shape': 'triangle',
-			'arrow-scale': 1.3,
-			'line-color': 'black',
-			'target-arrow-color': 'black'
+            'target-arrow-shape': 'triangle',
+            'arrow-scale': 1.3,
+            'line-color': 'black',
+            'target-arrow-color': 'black'
           }
         },
 
         {
           selector: 'edge.activation',
-		  style: {
-			'target-arrow-shape': 'circle',
-			'arrow-scale': 0.8,
-			'target-arrow-fill': 'hollow',
-			'line-style': 'dotted'
+          style: {
+            'target-arrow-shape': 'circle',
+            'arrow-scale': 0.8,
+            'target-arrow-fill': 'hollow',
+            'line-style': 'dotted'
           }
         },
 
         {
           selector: 'edge.inhibition',
-		  style: {
-			'target-arrow-shape': 'tee',
-			'arrow-scale': 0.8,
-			'line-style': 'dashed'
+          style: {
+            'target-arrow-shape': 'tee',
+            'arrow-scale': 0.8,
+            'line-style': 'dashed'
           }
         },
 
         {
           selector: 'edge.reactant',
-		  style: {
-			'target-arrow-shape': 'none',
+          style: {
+            'target-arrow-shape': 'none',
           }
         }
       ],
